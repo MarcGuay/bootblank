fe814b4dde0695ade16581906dc51fd3cd1e980c
Marc Guay <marc.guay@gmail.com>
2018-01-03 18:17:26 -0500

    Remove fontawesome

--- functions.php
+++ functions.php
@@ -95,12 +95,6 @@ function _tk_scripts() {
 	// load bootstrap css
 	wp_enqueue_style( '_tk-bootstrap', get_template_directory_uri() . '/assets/resources/bootstrap/css/bootstrap.min.css' );
 
-	// load Font Awesome css
-	wp_enqueue_style( '_tk-font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', false, '4.1.0' );
-
-	// load _tk styles
-	wp_enqueue_style( '_tk-style', get_stylesheet_uri() );
-
 	// load bootstrap js
 	wp_enqueue_script('_tk-bootstrapjs', get_template_directory_uri().'/assets/resources/bootstrap/js/bootstrap.min.js', array('jquery') );
 
