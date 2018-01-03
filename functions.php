98f33c493573ee8ef32b6de55afb659c79e6d829
Marc Guay <marc.guay@gmail.com>
2018-01-03 18:19:32 -0500

    Remove sidebar

--- functions.php
+++ functions.php
@@ -52,21 +52,6 @@ endif; // _tk_setup
 add_action( 'after_setup_theme', '_tk_setup' );
 
 /**
- * Register widgetized area and update sidebar with default widgets
- */
-function _tk_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', '_tk' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-		) );
-}
-add_action( 'widgets_init', '_tk_widgets_init' );
-
-/**
  * Enqueue scripts and styles
  */
 function _tk_scripts() {
