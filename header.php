a8838be9429bdfab3444258e7f4d5d2aa40138df
Marc Guay <marc.guay@gmail.com>
2021-08-17 12:10:13 -0400

    Replace hardcoded placeholder with the_content()

--- header.php
+++ header.php
@@ -22,41 +22,39 @@
     </div>
 </header>
 
-<nav class="site-navigation">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="navbar navbar-default">
-                    <div class="navbar-header">
-                        <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-
-                        <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-                    </div>
-
-                    <?php wp_nav_menu(
-                        array(
-                            'theme_location'    => 'primary',
-                            'depth'             => 2,
-                            'container'         => 'div',
-                            'container_class'   => 'collapse navbar-collapse',
-                            'menu_class'        => 'nav navbar-nav',
-                            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                            'menu_id'           => 'main-menu',
-                            'walker'            => new wp_bootstrap_navwalker()
-                        )
-                    ); ?>
-
-                </div>
+<div class="navbar-container">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#main-menu-container" aria-controls="main-menu-container" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-label"><?php _e('Menu', '_tk') ?></span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div id="main-menu-container" class="navbar-collapse offcanvas-collapse">
+            
+            <div class="mobile-menu-header d-lg-none">
+                <a href="#" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#main-menu-container" aria-controls="main-menu-container" aria-expanded="true" aria-label="Toggle navigation">×</a>
+                <?php get_search_form() ?>
             </div>
+            
+            <?php wp_nav_menu(
+                array(
+                    'theme_location'    => 'primary',
+                    'depth'             => 2,
+                    'container'         => false,
+                    'menu_class'        => 'navbar-nav',
+                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                    'menu_id'           => 'main-menu',
+                    'walker'            => new wp_bootstrap_navwalker()
+                )
+            ); ?>
         </div>
-    </div>
-</nav>
+      </div>
+    </nav>
+</div>
 
 <div id="main-content">
     <div class="container">
--- index.php
+++ index.php
@@ -15,13 +15,11 @@ get_header(); ?>
     <?php if ( have_posts() ) : ?>
 
         <?php while ( have_posts() ) : the_post(); ?>
-            Content placeholder
+            <?php the_content(); ?>
         <?php endwhile; ?>
 
     <?php else : ?>
-
         <?php get_template_part( 'no-results', 'index' ); ?>
-
     <?php endif; ?>
 
 <?php get_footer(); ?>
