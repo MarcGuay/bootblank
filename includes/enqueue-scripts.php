370fc97a78eff7bcfdb89daa8cf028f708cb5930
Marc Guay <marc.guay@gmail.com>
2021-08-17 11:41:31 -0400

    Don't need to enqueue jquery in theme

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -4,13 +4,10 @@
  */
 function bootblank_scripts() {
 
-    // enqueue jquery
-    wp_enqueue_script('jquery');
-    
     // load Bootstrap 
     wp_enqueue_style( 'bootblank-bootstrap', THEME_DIR_URI . '/assets/resources/bootstrap/css/bootstrap.min.css' );
     wp_enqueue_script('bootblank-bootstrapjs', THEME_DIR_URI . '/assets/resources/bootstrap/js/bootstrap.bundle.min.js', array('jquery'), null, true );
-    
+
     // load theme styles last so we can override
     wp_enqueue_style( 'theme-style', THEME_DIR_URI . '/assets/css/styles.css', null, filemtime(THEME_DIR_PATH.'/assets/css/styles.css') );
 
